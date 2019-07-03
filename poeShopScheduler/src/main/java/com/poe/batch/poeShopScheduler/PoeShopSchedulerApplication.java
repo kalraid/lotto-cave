cd388e3767fd88f080aad608bfd33e5570b02055
kalraid <kalraid@gmail.com>
2019-07-04 00:40:53 +0900

    https://jojoldu.tistory.com/325?category=635883 https://github.com/jojoldu/spring-batch-in-action https://spring.io/projects/spring-boot

---
+++ poeShopScheduler/src/main/java/com/poe/batch/poeShopScheduler/PoeShopSchedulerApplication.java
@@ -0,0 +1,13 @@
+package com.poe.batch.poeShopScheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoeShopSchedulerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PoeShopSchedulerApplication.class, args);
+	}
+
+}
---
+++ poeShopScheduler/src/test/java/com/poe/batch/poeShopScheduler/PoeShopSchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poe.batch.poeShopScheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PoeShopSchedulerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
